45d3bdd6f141b0078df5c265f6647fae3ef1f256
77ramesh <47748256+77ramesh@users.noreply.github.com>
2020-02-11 10:08:18 +0545

    Add files via upload

--- ImageEditor/src/application/MainApp.java
+++ ImageEditor/src/application/MainApp.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.io.IOException;
+
 public class MainApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		new MainFrame();
 
